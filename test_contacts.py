aa208a0966c54977c525e69fa641d83c7700e6a7
Casey Doran <cdoran2011@my.fit.edu>
2017-09-08 21:51:32 -0400

    add base unittest classes

---
+++ test_contacts.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestContacts(unittest.TestCase):
+    def test_thing1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_settings.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestSettings(unittest.TestCase):
+    def test_thing1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
