d6b72aa69c9aea2a3993530c843c9e75bd506f9a
Casey Doran <cdoran2011@my.fit.edu>
2017-09-28 10:56:39 -0400

    add a logout method

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -3,6 +3,13 @@ from selenium.webdriver.common.by import By
 
 class DashboardPage(PageBase):
     dashboard_selector = (By.CSS_SELECTOR, "div.dashboard")
+    logout_selector = (By.LINK_TEXT, "Logout")
     def initial_status(self):
         # make sure there is a dashboard div visible...
         self.driver.find_element(*self.dashboard_selector)
+
+    def log_out(self):
+        btn = self.driver.find_element(*self.logout_selector)
+        btn.click()
+        from .login import LoginPage
+        return LoginPage(self.driver)
