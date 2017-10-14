d2261defa1afbbb75158c3a384830eec7c92c655
Casey Doran <cdoran2011@my.fit.edu>
2017-10-13 22:26:09 -0400

    use driver from yeager walk call

--- yeager_test.py
+++ yeager_test.py
@@ -7,44 +7,37 @@ from yeager.annotations import state_transition
 driver = None
 
 @state_transition(None, "login-page")
-def open():
-    global driver
-    driver = webdriver.Chrome()
+def open(driver):
     driver.get("https://app.monicahq.com/")
 
 @state_transition("login-page", "dashboard-page")
-def log_in():
+def log_in(driver):
     login = LoginPage(driver)
     login.log_in_correctly()
 
 @state_transition(["dashboard-page", "contacts-page", "journal-page", "settings-page"], "login-page")
-def log_out():
+def log_out(driver):
     header = HeaderPage(driver)
     header.log_out()
 
 @state_transition(["dashboard-page", "contacts-page", "journal-page", "settings-page"], "dashboard-page")
-def dashboard_page():
+def dashboard_page(driver):
     header = HeaderPage(driver)
     header.go_dashboard()
 
 @state_transition(["dashboard-page", "contacts-page", "journal-page", "settings-page"], "contacts-page")
-def contacts_page():
+def contacts_page(driver):
     header = HeaderPage(driver)
     header.go_contacts()
 
 @state_transition(["dashboard-page", "contacts-page", "journal-page", "settings-page"], "journal-page")
-def journal_page():
+def journal_page(driver):
     header = HeaderPage(driver)
     header.go_journal()
 
 @state_transition(["dashboard-page", "contacts-page", "journal-page", "settings-page"], "settings-page")
-def settings_page():
+def settings_page(driver):
     header = HeaderPage(driver)
     header.go_settings()
 
-# @state_transition("login-page", None)
-def close():
-    global driver
-    driver.close()
-
-walk(50)
+walk(50, driver=webdriver.Chrome())
