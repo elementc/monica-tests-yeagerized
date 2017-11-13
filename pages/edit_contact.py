2f48fbba639a34b6677b9c0c798fca48ceb4c8d6
Casey Doran <cdoran2011@my.fit.edu>
2017-11-13 17:28:21 -0500

    start stubbing page object methods.

--- pages/edit_contact.py
+++ pages/edit_contact.py
@@ -4,6 +4,13 @@ from selenium.webdriver.common.alert import Alert
 
 class EditContactPage(HeaderPage):
     delete_btn_sel = (By.LINK_TEXT, "click here")
+    first_name_field_sel = (By.NAME, "first_name")
+    last_name_field_sel = (By.NAME, "last_name")
+    gender_none_radio_sel = (By.ID, "none")
+    gender_male_radio_sel = (By.ID, "male")
+    gender_female_radio_sel = (By.ID, "female")
+    cancel_button_sel = (By.LINK_TEXT, "Cancel")
+    save_button_sel = (By.CSS_SELECTOR, "form button.btn.btn-primary")
 
     def delete_this_contact(self):
         delete_btn = self.driver.find_element(*self.delete_btn_sel)
@@ -11,3 +18,90 @@ class EditContactPage(HeaderPage):
         Alert(self.driver).accept()
         from .contacts import ContactsPage
         return ContactsPage(self.driver)
+
+    def set_gender_male(self):
+        pass
+
+    def set_gender_male(self):
+        pass
+
+    def set_gender_female(self):
+        pass
+
+    def set_first_name(self, fname):
+        pass
+
+    def set_last_name(self, lname):
+        pass
+
+    def set_street(self, sname):
+        pass
+
+    def set_city(self, cname):
+        pass
+
+    def set_province(self, pname):
+        pass
+
+    def set_postcode(self, pcode):
+        pass
+
+    def set_country(self, cname):
+        pass
+
+    def set_email_address(self, ename):
+        pass
+
+    def set_phone(self, pname):
+        pass
+
+    def set_facebook(self, fname):
+        pass
+
+    def set_twitter(self, tname):
+        pass
+
+    def set_dont_know_age(self):
+        pass
+
+    def set_probable_age(self, p_age):
+        pass
+
+    def set_birthdate(self, bdate):
+        pass
+
+    def set_deceased_status(self, isdeceased):
+        pass
+
+    def set_is_deceased(self):
+        pass
+
+    def set_is_not_deceased(self):
+        pass
+
+    def set_dont_know_deceased_date(self):
+        pass
+
+    def set_know_deceased_date_status(self, know_deceased_date):
+        pass
+
+    def set_know_deceased_date(self):
+        pass
+
+    def set_deceased_date(self, date):
+        pass
+
+    def set_add_deceased_reminder_status(self, add_reminder):
+        pass
+
+    def set_add_deceased_reminder(self):
+        pass
+
+    def set_dont_add_deceased_reminder(self):
+        pass
+
+    def click_cancel_button(self):
+        pass
+
+    def click__save_button(self):
+        pass
