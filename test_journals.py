40ae47fd309ede016f5cf1431bb584ef51450506
Casey Doran <cdoran2011@my.fit.edu>
2017-11-29 03:05:19 -0500

    remove traditional code from yeager repo

--- test_journals.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestJournals(unittest.TestCase):
-    def test_thing1(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
--- test_settings.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestSettings(unittest.TestCase):
-    def test_thing1(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
